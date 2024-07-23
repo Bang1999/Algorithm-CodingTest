47a39ee8703ec4c7e619f1cbd3fe0e755500b48d
DongHo Bang <74201427+Bang1999@users.noreply.github.com>
2024-07-24 01:46:52 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 144 ms, Memory: 15916 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        br.close();
+        int[] cnt = new int[26];
+        for(int i=0;i<str.length();i++){
+            cnt[str.charAt(i) - 'a']++;
+        }
+        for(int i=0;i<cnt.length;i++){
+            System.out.print(cnt[i] + " ");
+        }
+        
+        
+    }
+}
