32db3af98458800175184fb4e460066732e8da3e
DongHo Bang <74201427+Bang1999@users.noreply.github.com>
2024-07-31 16:26:04 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int A;
+    int B;
+    
+    scanf("%d%d", &A,&B);
+    printf("%d\n", A*(B%10));
+    printf("%d\n", A*((B%100)-(B%10))/10);
+    printf("%d\n", A*(B-(B%100))/100);
+    printf("%d\n", A*B);
+    return 0;
+}
