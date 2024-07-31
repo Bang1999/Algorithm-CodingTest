879d7e1cb6c77d43d842bef37f9e47e805d9304a
DongHo Bang <74201427+Bang1999@users.noreply.github.com>
2024-07-31 16:20:55 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int A;
+    int B;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    printf("%.10f", (double)A/B);
+    
+    
+    return 0;
+}
