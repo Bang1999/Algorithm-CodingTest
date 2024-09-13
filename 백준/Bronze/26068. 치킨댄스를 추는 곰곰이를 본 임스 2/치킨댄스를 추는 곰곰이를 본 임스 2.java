136dc5dde93e9f103e64b69a121c1957ae1cffb4
DongHo Bang <74201427+Bang1999@users.noreply.github.com>
2024-09-14 00:55:44 +0900

    [Bronze IV] Title: 치킨댄스를 추는 곰곰이를 본 임스 2, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/26068. 치킨댄스를 추는 곰곰이를 본 임스 2/치킨댄스를 추는 곰곰이를 본 임스 2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine());
+        int ans = 0;
+        for(int i=0;i<T;i++){
+            String str = br.readLine();
+            
+            if(Integer.parseInt(str.substring(2)) <= 90){
+                ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+}
