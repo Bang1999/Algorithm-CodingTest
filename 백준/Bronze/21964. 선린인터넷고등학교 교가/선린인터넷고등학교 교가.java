314e2939f231a4f53b1e0e3553ba7c91c713407f
DongHo Bang <74201427+Bang1999@users.noreply.github.com>
2024-09-22 12:39:14 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 116 ms, Memory: 15412 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        
+        System.out.println(str.substring(str.length()-5));
+    }
+}
