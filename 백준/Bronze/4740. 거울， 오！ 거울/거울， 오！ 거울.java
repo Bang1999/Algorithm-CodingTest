24d9e1de26fddacef9fff1bec7df0a9beaaa385b
DongHo Bang <74201427+Bang1999@users.noreply.github.com>
2024-09-24 01:12:18 +0900

    [Bronze III] Title: 거울, 오! 거울, Time: 100 ms, Memory: 13992 KB -BaekjoonHub

---
+++ 백준/Bronze/4740. 거울， 오！ 거울/거울， 오！ 거울.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String str = br.readLine();
+        while(!"***".equals(str)){
+            for(int i=str.length()-1;i>=0;i--){
+                System.out.print(str.charAt(i));
+            }
+            System.out.println();
+            str = br.readLine();
+        }
+    }
+}
