4f60ac018b629ed94cafb988619f839db9cf6390
DongHo Bang <74201427+Bang1999@users.noreply.github.com>
2024-11-27 14:49:32 +0900

    [level 0] Title: 더 크게 합치기, Time: 15.09 ms, Memory: 68.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        String A = ""+a+b;
+        String B = ""+b+a;
+        if(Integer.parseInt(A)>Integer.parseInt(B)){
+            answer = Integer.parseInt(A);
+        }else{
+            answer = Integer.parseInt(B);
+        }
+        return answer;
+    }
+}
