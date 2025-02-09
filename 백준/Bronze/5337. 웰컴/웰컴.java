9b463b7c3aa07223de8daeb97ce5928ddd5543ac
DongHo Bang <74201427+Bang1999@users.noreply.github.com>
2025-02-10 02:02:01 +0900

    [Bronze V] Title: 웰컴, Time: 96 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .\n");
+        sb.append("|  | _ | _. _ ._ _  _\n");
+        sb.append("|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.println(sb);
+    }
+}
