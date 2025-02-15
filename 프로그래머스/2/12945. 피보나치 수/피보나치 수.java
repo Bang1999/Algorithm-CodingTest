a1a9cb20f8d2b6a9c74ce3ee3001ffe11244fc24
DongHo Bang <74201427+Bang1999@users.noreply.github.com>
2025-02-15 20:14:15 +0900

    [level 2] Title: 피보나치 수, Time: 2.89 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if(n <= 2){
+            return 1;
+        }
+        int[] arr = new int[n+1];
+        arr[1] = 1;
+        arr[2] = 1;
+        for(int i=3;i<=n;i++){
+            arr[i] = arr[i-2] + arr[i-1];
+            arr[i] %= 1234567;
+        }
+        return arr[n] % 1234567;
+    }
+}
