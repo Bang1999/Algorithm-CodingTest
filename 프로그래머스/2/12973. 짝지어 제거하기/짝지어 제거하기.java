cd9186999e662e98115be5cd34054a13d69ce9ce
DongHo Bang <74201427+Bang1999@users.noreply.github.com>
2025-03-20 14:53:37 +0900

    [level 2] Title: 짝지어 제거하기, Time: 66.22 ms, Memory: 63.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution { 
+    public int solution(String s) {
+        if(s.length() == 0){
+            return 1;
+        }
+        Stack<Character> st = new Stack<>();
+        st.push(s.charAt(0));
+        for(int i=1;i<s.length();i++){
+            if(!st.isEmpty() && st.peek() == s.charAt(i)){
+                st.pop();
+            }else{
+                st.push(s.charAt(i));
+            }
+        }
+        if(st.isEmpty()){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
