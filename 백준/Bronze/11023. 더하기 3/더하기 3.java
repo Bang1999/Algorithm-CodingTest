90f34d1e525450fdfde89af51ddb2ce7612d1b8e
DongHo Bang <74201427+Bang1999@users.noreply.github.com>
2025-03-26 11:57:13 +0900

    [Bronze III] Title: 더하기 3, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/11023. 더하기 3/더하기 3.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] arr = br.readLine().split(" ");
+        int sum = 0;
+        for(int i=0;i<arr.length;i++) {
+            sum += Integer.parseInt(arr[i]);
+        }
+        System.out.println(sum);
+    }
+}
