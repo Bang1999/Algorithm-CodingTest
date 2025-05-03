1b72d00a66f1448c29fba72af98cbe0ceedba696
DongHo Bang <74201427+Bang1999@users.noreply.github.com>
2025-05-03 16:31:05 +0900

    [level 0] Title: A로 B 만들기, Time: 2.81 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String before, String after) {
+        int answer = 1;
+        List<Character> list = new ArrayList<>();
+        for(int i=0;i<after.length();i++) {
+            list.add(after.charAt(i));
+        }
+        
+        for(int i=0;i<before.length();i++) {
+            char c = before.charAt(i);
+            if(list.contains(c)) {
+                list.remove((Character) c);
+            } else {
+                return 0;
+            }
+        }
+        
+        return answer;
+    }
+}
