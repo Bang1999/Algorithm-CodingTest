3e2acda2c1569de5615347f3a822e494cd639d8e
DongHo Bang <74201427+Bang1999@users.noreply.github.com>
2025-08-11 18:15:04 +0900

    [Silver IV] Title: 다음 소수, Time: 184 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            long n = Long.parseLong(br.readLine());
+            while (!isPrime(n)) {
+                n++;
+            }
+            sb.append(n).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    static boolean isPrime(long num) {
+        if (num < 2) return false;
+        if (num == 2 || num == 3) return true;
+        if (num % 2 == 0 || num % 3 == 0) return false;
+
+        long sqrt = (long) Math.sqrt(num);
+        for (long i = 5; i <= sqrt; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
